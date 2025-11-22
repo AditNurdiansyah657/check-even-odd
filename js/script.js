d439982b080a6b13676920c5d12232c9cf717993
Adit Nurdiansyah <aditnurdiansyah09@gmail.com>
2025-11-22 19:46:34 +0700

    Reformat cekAngka function and add kirimAnonim

--- js/script.js
+++ js/script.js
@@ -19,4 +19,18 @@ function cekAngka() {
     } else {
         hasil.textContent = `${angka} adalah angka Ganjil`;
     }
+
+}
+
+// KONTAK 
+function kirimAnonim(event) {
+    event.preventDefault();
+    var isi = document.getElementById('kritikSaran').value.trim();
+    if(isi) {
+        document.getElementById('pesanAnonim').textContent = 'Terima kasih atas kritik & saran Anda!';
+        document.getElementById('formAnonim').reset();
+    } else {
+        document.getElementById('pesanAnonim').textContent = '';
+    }
+    return false;
 }
